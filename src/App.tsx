00b90469389e28cd4d9f462eedbbf221f67eac95
godakid <godakid@outlook.com>
2024-05-07 21:51:08 +0800

    camera test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import { useState, useRef } from 'react'
-import './App.css'
-import { Camera } from "react-camera-pro";
+import {useRef, useState} from 'react';
+import './App.css';
+import {Camera} from "react-camera-pro";
+
 
 function App() {
 
-  const camera = useRef<any>(null);
-  const [image, setImage] = useState<string>("");
-  return (
-    <>
-      <div className='flex flex-col'>
-        <Camera ref={camera}
-          errorMessages={{
-          }}
-        />
-        <button onClick={() => setImage(camera.current!.takePhoto())}>Take photo</button>
-        {image && <img src={image!} alt='Taken photo' />}
-      </div>
-    </>
-  )
+    const camera = useRef<any>(null);
+    const [image, setImage] = useState<string>("");
+
+    return (
+        <>
+            <div className="flex flex-col  ">
+                <Camera ref={camera}
+                        errorMessages={{
+                            noCameraAccessible: 'No camera device accessible. Please connect your camera or try a different browser.',
+                            permissionDenied: 'Permission denied. Please refresh and give camera permission.',
+                            switchCamera:
+                                'It is not possible to switch camera to different one because there is only one video device accessible.',
+                            canvas: 'Canvas is not supported.'
+                        }}
+
+                />
+                <button className={'absolute rounded-full w-5em h-5em outline-none border-none drop-shadow-lg bottom-4em left-50% -translate-x-50%'}
+                        onClick={() => setImage(camera.current!.takePhoto())}/>
+                {image && <img src={image!} alt="Taken photo"/>}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,48 @@
-import { ConfigEnv, defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {ConfigEnv, defineConfig} from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import Unocss from "unocss/vite";
 
-import { viteVConsole } from 'vite-plugin-vconsole';
+import {viteVConsole} from 'vite-plugin-vconsole';
 import * as path from 'path';
-import { VitePWA } from 'vite-plugin-pwa';
+import {VitePWA} from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }: ConfigEnv) => ({
-  plugins: [
-    react(),
-    Unocss({}),
-    viteVConsole({
-      entry: path.resolve('src/main.tsx'), // or you can use entry: [path.resolve('src/main.ts')]
-      enabled: command !== 'serve' || mode === 'test',
-      config: {
-        maxLogNumber: 1000,
-        theme: 'dark'
-      }
-    }),
-    VitePWA({
-      // 插件配置项
-      manifest: {
-        "name": 'PWA Demo',
-        "description": "A PWA demo built with Vite and vite pwa",
-        "theme_color": "#242424",
-        // 为了方便，使用svg图标
-        icons: [
-          {
-            "src": "/vite.svg",
-            "sizes": "192x192",
-            "type": "image/svg+xml"
-          },
-          {
-            "src": "/vite.svg",
-            "sizes": "512x512",
-            "type": "image/svg+xml"
-          }
-        ]
-      },
-    }),
-  ],
-}))
+export default defineConfig(({command, mode}: ConfigEnv) => ({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        }
+    },
+    plugins: [
+        react(),
+        Unocss({}),
+        viteVConsole({
+            entry: path.resolve('src/main.tsx'), // or you can use entry: [path.resolve('src/main.ts')]
+            enabled: command !== 'serve' || mode === 'test',
+            config: {
+                maxLogNumber: 1000,
+                theme: 'dark'
+            }
+        }),
+        VitePWA({
+            // 插件配置项
+            manifest: {
+                "name": 'PWA Demo',
+                "description": "A PWA demo built with Vite and vite pwa",
+                "theme_color": "#242424",
+                // 为了方便，使用svg图标
+                icons: [
+                    {
+                        "src": "/vite.svg",
+                        "sizes": "192x192",
+                        "type": "image/svg+xml"
+                    },
+                    {
+                        "src": "/vite.svg",
+                        "sizes": "512x512",
+                        "type": "image/svg+xml"
+                    }
+                ]
+            },
+        }),
+    ],
+}));
