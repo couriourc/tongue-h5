db4338c1bb213a0067d8a0fb8ac678bae1fd13e8
chenrun <godakid@outlook.com>
2024-05-08 12:58:05 +0800

    overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {useRef, useState} from 'react';
 import './App.css';
 import {Camera} from "react-camera-pro";
-
+import {CiCamera} from "react-icons/ci";
 
 function App() {
 
@@ -10,7 +10,7 @@ function App() {
 
     return (
         <>
-            <div className="flex flex-col  ">
+            <div className="flex flex-col h-screen w-screen overflow-hidden">
                 <Camera ref={camera}
                         errorMessages={{
                             noCameraAccessible: 'No camera device accessible. Please connect your camera or try a different browser.',
@@ -21,8 +21,16 @@ function App() {
                         }}
 
                 />
-                <button className={'absolute rounded-full w-5em h-5em outline-none border-none drop-shadow-lg bottom-4em left-50% -translate-x-50%'}
-                        onClick={() => setImage(camera.current!.takePhoto())}/>
+                <div className={'fixed bg-[#0000008e] h-screen w-screen pointer-events-none z-0'}>
+                </div>
+                <button
+                    className={`
+                    absolute rounded-full bg-transparent  border-1px
+                    w-5em h-5em outline-none border-none bottom-4em left-50% -translate-x-50% 
+                    hover:border-solid cursor-pointer z-100`}
+                    onClick={() => setImage(camera.current!.takePhoto())}>
+                    <CiCamera size={40} color={'#FFF'}/>
+                </button>
                 {image && <img src={image!} alt="Taken photo"/>}
             </div>
         </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 import 'uno.css';
+import {ConfigProvider} from 'react-vant';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ConfigProvider>
+            <App/>
+        </ConfigProvider>
+    </React.StrictMode>,
+);
