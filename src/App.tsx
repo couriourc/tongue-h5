f7f6e74e1329cd044a0a1f902e2b07ad8c81afe5
chenrun <godakid@outlook.com>
2024-05-08 18:32:21 +0800

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import {useRef, useState} from 'react';
 import './App.css';
-import {Camera} from "react-camera-pro";
-import {CiCamera} from "react-icons/ci";
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Capture} from "./views/capture/Capture";
 
 function App() {
-
-    const camera = useRef<any>(null);
-    const [image, setImage] = useState<string>("");
-
-    return (
-        <>
-            <div className="flex flex-col h-screen w-screen overflow-hidden">
-                <Camera ref={camera}
-                        errorMessages={{
-                            noCameraAccessible: 'No camera device accessible. Please connect your camera or try a different browser.',
-                            permissionDenied: 'Permission denied. Please refresh and give camera permission.',
-                            switchCamera:
-                                'It is not possible to switch camera to different one because there is only one video device accessible.',
-                            canvas: 'Canvas is not supported.'
-                        }}
-
-                />
-                <div className={'fixed bg-[#0000008e] h-screen w-screen pointer-events-none z-0'}>
-                </div>
-                <button
-                    className={`
-                    absolute rounded-full bg-transparent  border-1px
-                    w-5em h-5em outline-none border-none bottom-4em left-50% -translate-x-50% 
-                    hover:border-solid cursor-pointer z-100`}
-                    onClick={() => setImage(camera.current!.takePhoto())}>
-                    <CiCamera size={40} color={'#FFF'}/>
-                </button>
-                {image && <img src={image!} alt="Taken photo"/>}
-            </div>
-        </>
-    );
+    return <>
+        <BrowserRouter basename={"/"}>
+            <Routes>
+                <Route path="/" element={<Capture/>}/> {/* 👈 Renders at /app/ */}
+            </Routes>
+        </BrowserRouter>
+    </>;
 }
 
 export default App;
---
+++ src/views/capture/Capture.tsx
@@ -0,0 +1,37 @@
+import {useRef, useState} from "react";
+import {Camera} from "react-camera-pro";
+import {CiCamera} from "react-icons/ci";
+
+export function Capture(){
+
+    const camera = useRef<any>(null);
+    const [image, setImage] = useState<string>("");
+
+    return (
+        <>
+            <div className="flex flex-col h-screen w-screen overflow-hidden touch-none">
+                <Camera ref={camera}
+                        errorMessages={{
+                            noCameraAccessible: 'No camera device accessible. Please connect your camera or try a different browser.',
+                            permissionDenied: 'Permission denied. Please refresh and give camera permission.',
+                            switchCamera:
+                                'It is not possible to switch camera to different one because there is only one video device accessible.',
+                            canvas: 'Canvas is not supported.'
+                        }}
+
+                />
+                <div className={'fixed bg-[#0000008e] h-screen w-screen pointer-events-none z-0'}>
+                </div>
+                <button
+                    className={`
+                    absolute rounded-full bg-transparent  border-1px
+                    w-5em h-5em outline-none border-none bottom-4em left-50% -translate-x-50% 
+                    hover:border-solid cursor-pointer z-100`}
+                    onClick={() => setImage(camera.current!.takePhoto())}>
+                    <CiCamera size={40} color={'#FFF'}/>
+                </button>
+                {image && <img src={image!} alt="Taken photo"/>}
+            </div>
+        </>
+    );
+}
