d5ecc1202b463ab05894600891fd5b680082181f
couriourc <58319769+couriourc@users.noreply.github.com>
2024-06-05 12:24:56 +0000

    Deploying to release from @ couriourc/tongue-h5@1047b68ae011cfb4dd7c3967fb731be88dc690c2 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tongue-h5/sw.js', { scope: '/tongue-h5/' })})}
